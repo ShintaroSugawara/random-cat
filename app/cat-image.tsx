baea0a1c9d4a8d04f8d43fc22af90459a391f7be
ShintaroSugawara <sugawgra.shintaro@gmail.com>
2026-09-08 16:35:19 +0900

    ver.1

---
+++ app/cat-image.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useState } from "react";
+import { fetchImage } from "./fetch-image";
+
+type CatImageProps = {
+    url: string;
+};
+
+export function CatImage({ url }: CatImageProps) {
+    const [imageUrl, setImageUrl] = useState(url);
+
+    const refreshImage = async () => {
+        setImageUrl("");
+        const image = await fetchImage();
+        setImageUrl(image.url);
+    }
+    return (
+        <div>
+            <button onClick={refreshImage}>他のにゃんこも見る</button>
+
+            {imageUrl && <img src={imageUrl} />}
+        </div>
+    );
+}
